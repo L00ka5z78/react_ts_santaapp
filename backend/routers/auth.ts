1b01171b77c47e4bf7fe565f2a04a6a443fe6f14
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-01-01 10:50:12 +0100

    trying to add auth logic first time in my life. difficult

---
+++ backend/routers/auth.ts
@@ -0,0 +1,9 @@
+import * as express from 'express';
+import { register, login, logout } from '../controllers/auth.js';
+
+export const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+router.post('/logout', logout);
+//1.06'
